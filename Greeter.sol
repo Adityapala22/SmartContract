68c05443ad2e784cfbc15e377045194848b57736
Adityapala22 <96135760+Adityapala22@users.noreply.github.com>
2021-12-16 19:58:16 +0000

    Update Greeter.sol

--- Greeter.sol
+++ Greeter.sol
@@ -4,7 +4,7 @@ contract Greeter {
  string public greeting;
 
  constructor() public {
-  greeting = 'Hello';
+  greeting = 'Hello Paul, Merry Christmas. From Adi 10584842';
  }
 
  function setGreeting(string memory _greeting) public {
